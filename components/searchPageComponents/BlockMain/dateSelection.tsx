cd738fb164b5f8a5cf3e1dc1d6b3a7e03fd72d91
Iakov <kirbabin.jacob@gmail.com>
2025-09-14 19:06:47 +0300

    fast

--- components/searchPageComponents/BlockMain/dateSelection.tsx
+++ components/searchPageComponents/BlockMain/dateSelection.tsx
@@ -2,62 +2,90 @@ import { useState, forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import st from './styles.module.scss';
+import Image from 'next/image';
+import rectangleIcon from '@/public/rectangleIcon.svg';
+import clsx from 'clsx';
 
 export const DateSelection = () => {
-	const [dateTo, setDateTo] = useState<Date | null>(new Date());
-	const [dateFrom, setDateFrom] = useState<Date | null>(new Date());
+	const [dateTo, setDateTo] = useState<Date | null>(null);
+	const [dateFrom, setDateFrom] = useState<Date | null>(null);
+	const [isOpenFrom, setIsOpenFrom] = useState(false);
+	const [isOpenTo, setIsOpenTo] = useState(false);
 
 	return (
 		<div className={st.chooseDate}>
 			<DatePicker
 				selected={dateTo}
 				onChange={(date) => setDateTo(date)}
-				customInput={<CustomInputFrom />}
+				customInput={<CustomInputFrom rotate={isOpenFrom} />}
 				dateFormat="dd.MM.yyyy"
-				placeholderText="Выберите дату"
+				calendarClassName={st.castomCalendar}
+				popperPlacement="top-start"
+				onCalendarOpen={() => setIsOpenFrom(true)}
+				onCalendarClose={() => setIsOpenFrom(false)}
 			/>
+
 			<DatePicker
 				selected={dateFrom}
 				onChange={(date) => setDateFrom(date)}
-				customInput={<CustomInputTo />}
+				customInput={<CustomInputTo rotate={isOpenTo} />}
 				dateFormat="dd.MM.yyyy"
-				placeholderText="Выберите дату"
+				calendarClassName={st.castomCalendar}
+				popperPlacement="top-start"
+				onCalendarOpen={() => setIsOpenTo(true)}
+				onCalendarClose={() => setIsOpenTo(false)}
 			/>
 		</div>
 	);
 };
 
-// Кастомные инпуты (через форвард реф тк по другому библиотека не поддерживает)
-const CustomInputFrom = forwardRef<
-	HTMLInputElement,
-	React.HTMLProps<HTMLInputElement>
->(({ value, onClick, onChange }, ref) => (
-	<input
-		name="dateFrom"
-		className={st.customInput}
-		onClick={onClick}
-		onChange={onChange}
-		value={value}
-		ref={ref}
-		readOnly
-	/>
-));
+interface CustomInputProps extends React.HTMLProps<HTMLInputElement> {
+	rotate: boolean;
+}
 
+const CustomInputFrom = forwardRef<HTMLInputElement, CustomInputProps>(
+	({ value, onClick, onChange, rotate }, ref) => (
+		<>
+			<input
+				name="dateFrom"
+				className={st.customInput}
+				onClick={onClick}
+				onChange={onChange}
+				value={value}
+				ref={ref}
+				readOnly
+				placeholder="Дата начала"
+				style={{ marginRight: '20px' }}
+			/>
+			<Image
+				className={clsx(st.firstrectangle, { [st.rotateRectangle]: rotate })}
+				src={rectangleIcon}
+				alt="треугольник"
+			/>
+		</>
+	)
+);
 CustomInputFrom.displayName = 'CustomInputFrom';
 
-const CustomInputTo = forwardRef<
-	HTMLInputElement,
-	React.HTMLProps<HTMLInputElement>
->(({ value, onClick, onChange }, ref) => (
-	<input
-		name="dateTo"
-		className={st.customInput}
-		onClick={onClick}
-		onChange={onChange}
-		value={value}
-		ref={ref}
-		readOnly
-	/>
-));
-
+const CustomInputTo = forwardRef<HTMLInputElement, CustomInputProps>(
+	({ value, onClick, onChange, rotate }, ref) => (
+		<>
+			<input
+				name="dateTo"
+				className={st.customInput}
+				onClick={onClick}
+				onChange={onChange}
+				value={value}
+				ref={ref}
+				readOnly
+				placeholder="Дата конца"
+			/>
+			<Image
+				className={clsx(st.secondRectangle, { [st.rotateRectangle]: rotate })}
+				src={rectangleIcon}
+				alt="треугольник"
+			/>
+		</>
+	)
+);
 CustomInputTo.displayName = 'CustomInputTo';
--- components/searchPageComponents/BlockMain/tonality.tsx
+++ components/searchPageComponents/BlockMain/tonality.tsx
@@ -1,11 +1,16 @@
+import st from './styles.module.scss';
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { useState } from 'react';
+import rectangleIcon from '@/public/rectangleIcon.svg';
+import clsx from 'clsx';
+import Image from 'next/image';
 
 export const Tonality = () => {
 	const [ton, setTon] = useState('Любая');
+	const [rotate, setRotate] = useState(false);
 
 	const handleChange = (event: SelectChangeEvent) => {
 		setTon(event.target.value as string);
@@ -18,8 +23,12 @@ export const Tonality = () => {
 					name="tonality"
 					value={ton}
 					onChange={handleChange}
+					onClick={() => setRotate((prev) => !prev)}
 					MenuProps={{ disableScrollLock: true }}
 					variant="outlined"
+					IconComponent={(props) => (
+						<CustomArrowIcon {...props} rotate={rotate} />
+					)}
 					sx={{
 						width: 242,
 						height: 43,
@@ -34,5 +43,25 @@ export const Tonality = () => {
 				</Select>
 			</FormControl>
 		</Box>
+	);
+};
+
+const CustomArrowIcon = (props: { className?: string; rotate?: boolean }) => {
+	const rotate = false;
+
+	return (
+		<Image
+			className={clsx(props.className, {
+				[st.rotateRectangle]: rotate,
+			})}
+			style={{
+				position: 'absolute',
+				top: '15px',
+				right: '16px',
+				zIndex: -1,
+			}}
+			src={rectangleIcon}
+			alt="треугольник"
+		/>
 	);
 };
