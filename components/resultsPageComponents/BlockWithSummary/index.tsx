baa9155ab57156053763a2ed454c47bc1b1eb11e
Iakov <kirbabin.jacob@gmail.com>
2025-09-15 22:55:11 +0300

    fast

--- components/resultsPageComponents/BlockWithSummary/index.tsx
+++ components/resultsPageComponents/BlockWithSummary/index.tsx
@@ -7,6 +7,7 @@ import { Navigation } from 'swiper/modules';
 import Image from 'next/image';
 import leftArrow from '@/public/leftArrow.svg';
 import rightArrow from '@/public/rightArrow.svg';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export const BlockWithSummary = () => {
 	return (
@@ -14,45 +15,53 @@ export const BlockWithSummary = () => {
 			<h2 className={st.subTitle}>ОБЩАЯ СВОДКА</h2>
 			<p className={st.underSubTitle}>Найдено {0} вариантов</p>
 			<div className={st.swiperContainer}>
-				<div className={st.fixedSlide}>
-					<p className={st.categories}>Период</p>
-					<p className={st.categories}>Всего</p>
-					<p className={st.categories}>Риски</p>
-				</div>
 				<div className={st.customPrev}>
 					<Image src={leftArrow} alt="<—" />
 				</div>
 				<Swiper
 					className={st.carousel}
-					spaceBetween={0}
-					slidesPerView={3}
-					slidesPerGroup={3}
+					slidesPerView={1}
+					slidesPerGroup={1}
 					loop={true}
 					modules={[Navigation]}
 					navigation={{
 						prevEl: `.${st.customPrev}`,
 						nextEl: `.${st.customNext}`,
 					}}>
-					<SwiperSlide className={st.slide}>
-						<p className={st.slideCategories}>дата</p>
-						<p className={st.slideCategories}>0</p>
-						<p className={st.slideCategories}>0</p>
-					</SwiperSlide>
-					<SwiperSlide className={st.slide}>
-						<p className={st.slideCategories}>дата</p>
-						<p className={st.slideCategories}>0</p>
-						<p className={st.slideCategories}>0</p>
-					</SwiperSlide>
-					<SwiperSlide className={st.slide}>
-						<p className={st.slideCategories}>дата</p>
-						<p className={st.slideCategories}>0</p>
-						<p className={st.slideCategories}>0</p>
-					</SwiperSlide>
-					<SwiperSlide className={st.slide}>
-						<p className={st.slideCategories}>дата</p>
-						<p className={st.slideCategories}>0</p>
-						<p className={st.slideCategories}>0</p>
-					</SwiperSlide>
+					<div className={st.fixedSlide}>
+						<p className={st.categories}>Период</p>
+						<p className={st.categories}>Всего</p>
+						<p className={st.categories}>Риски</p>
+					</div>
+					{1 ? (
+						<SwiperSlide>
+							<CircularProgress className={st.loader} thickness={5} />
+							<p className={st.underLoader}>Загружаем данные</p>
+						</SwiperSlide>
+					) : (
+						<>
+							<SwiperSlide className={st.slide}>
+								<p className={st.slideCategories}>дата</p>
+								<p className={st.slideCategories}>0</p>
+								<p className={st.slideCategories}>0</p>
+							</SwiperSlide>
+							<SwiperSlide className={st.slide}>
+								<p className={st.slideCategories}>дата</p>
+								<p className={st.slideCategories}>0</p>
+								<p className={st.slideCategories}>0</p>
+							</SwiperSlide>
+							<SwiperSlide className={st.slide}>
+								<p className={st.slideCategories}>дата</p>
+								<p className={st.slideCategories}>0</p>
+								<p className={st.slideCategories}>0</p>
+							</SwiperSlide>
+							<SwiperSlide className={st.slide}>
+								<p className={st.slideCategories}>дата</p>
+								<p className={st.slideCategories}>0</p>
+								<p className={st.slideCategories}>0</p>
+							</SwiperSlide>
+						</>
+					)}
 				</Swiper>
 				<div className={st.customNext}>
 					<Image src={rightArrow} alt="—>" />
